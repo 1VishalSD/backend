010a9b9006277c92a935ea2b22590ca1889ee8ed
1VishalSD <ilearnvk@gmail.com>
2024-06-13 12:38:45 +0530

    connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require("dotenv").config({ path: "./env" });
 
-import express from "express";
+import dotenv from "dotenv";
 
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+/* 
+  import express from "express";
 const app = express()(async () => {
   try {
     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
@@ -20,3 +31,4 @@ const app = express()(async () => {
     throw err;
   }
 })();
+*/
